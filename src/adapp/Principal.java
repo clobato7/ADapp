be3572444d8385cb74158175d75ba95c8ed0efea
Caio Lobato <Caio Lobato@DESKTOP-VVC9103>
2019-01-19 04:47:03 -0300

    final

--- src/adapp/Principal.java
+++ src/adapp/Principal.java
@@ -13,33 +13,44 @@ import java.sql.Statement;
 import java.util.Random;
 
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Observable;
 
 /**
  *
  * @author Caio Lobato
  */
-public class Principal {
+public class Principal extends Observable {
 
-    //Principal app = new Principal();
     /**
      * @param args the command line arguments
      */
-    
     public boolean conexao = true; // situação da "conexao"
-    
+
+    public Principal() {
+
+    }
+
     public boolean getConexao() {
         return this.conexao;
     }
 
     public void setConexao(boolean c) {
         this.conexao = c;
-        //setChanged();
-        //notifyObservers(name);
+
+        String status = "";
+        if (c == true) {
+            status = "ON";
+        } else if (c == false) {
+            status = "OFF";
+        }
+        setChanged();
+        notifyObservers(c);
     }
-    
-    
+
     private Connection connect(String db) {
         // SQLite connection string
         String url = "jdbc:sqlite:" + db;
@@ -50,6 +61,23 @@ public class Principal {
             System.out.println(e.getMessage());
         }
         return conn;
+    }
+
+    String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
     }
 
     public void insertFuncionario(String nome, int id_cargo) {
@@ -65,76 +93,91 @@ public class Principal {
         }
     }
 
-    boolean simInputData(boolean conex, int t, BufferedWriter w) throws IOException { // conex verifica a conexao | t numero de registros a simular o input
+    public void insertFuncionarioFromTxt(String sql) {
+   
+        String sqlArray[] = sql.split("\\r?\\n"); //separa os comandos em uma array pq se n ele executa só o primeiro 
+
+
+        try (Connection conn = this.connect("funcionario.db");) {
+            for (int i = 0; i < sqlArray.length; i++) {
+                PreparedStatement pstmt = conn.prepareStatement(sqlArray[i]);
+                pstmt.executeUpdate();
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println("Inseriu no banco o(s) registro(s) do temp.txt.");
+
+    }
+
+    public void simInputData(Principal a, int t, BufferedWriter w) throws IOException { // conex verifica a conexao | t numero de registros a simular o input
         int i = 1; // contar os loops
         Random gerador = new Random(); // para colocar id_cargo aleatorios no insert
 
-        //if (conex = true) {
-        while (conex == true) {
+
+        while (a.getConexao() == true) {
 
             insertFuncionario("Funcionario " + i, gerador.nextInt(5) + 1); // nome , id_cargo
 
             if (i == t) {
-                conex = false;
-                return conex;
+                //conex = false;
+                //return conex;
+                System.out.println("Inseriu no banco " + i + " registro(s).");
+                //a.setConexao(false);
+                return;
             } // simulando desconexão
 
             i++;
 
         }
 
-        if (conex == false) {
-            // cria o temp.txt se conex tiver off 
-            
+        while (a.getConexao() == false) {
 
-            //} else if (conex = false) {
-            while (conex == false) {
+            w.write("INSERT INTO funcionario(nome,id_cargo) VALUES('FuncionarioText " + i + "'," + (gerador.nextInt(5) + 1) + ");");
+            //w.write("FuncionarioTex " + i + "\t" + (gerador.nextInt(5) + 1));
+            w.newLine();
 
-                w.write("FuncionarioTex " + i + "\t" + (gerador.nextInt(5) + 1));
-                w.newLine();
-
-                if (i == t) {
-                    conex = true; // simulando reconexão
-                }
-                i++;
-
+            if (i == t) {
+                //conex = true; // simulando reconexão
+                w.close(); /* encerra o txt e salva nele, isso aqui eu to testando ainda, ele n deve ficar 
+         na class input pq se quiser botar mais de 2 inputs offline ele lá vai apagar o 1º*/
+                
+                System.out.println("Inseriu no temp.txt " + i + " registro(s).");
+                //setConexao(true);
+                return;
             }
-            //writer.close();// isso aqui encerra o input no txt // isso n deve encerrar aqui
+            i++;
+
         }
-        
-        
-        
-        return conex;
+
 
     }
 
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
 
-        
         Principal app = new Principal(); // para colocar id_cargo aleatorios no insert
-        
-        BufferedWriter writer = new BufferedWriter(new FileWriter(
-                    "temp.txt")); // setando o writer pro arquivo temp.txt
 
-        app.conexao = app.simInputData(app.conexao, 5, writer); // primeira leva de dados = 5 registros
+        BufferedWriter writer = new BufferedWriter(new FileWriter(
+                "temp.txt")); // setando o writer pro arquivo temp.txt
+
+        Monitoramento monit = new Monitoramento(app);
+
+        app.addObserver(monit);
+
+        app.simInputData(app, 5, writer); // primeira leva de dados = 5 registros
+        
         // desconexão aqui
+        app.setConexao(false);
+
+        app.simInputData(app, 7, writer); // segunda leva de dados = 7 registros
         
-        app.conexao = app.simInputData(app.conexao, 7, writer); // segunda leva de dados = 7 registros
         // reconexão aqui 
-        
-        app.conexao = app.simInputData(app.conexao, 5, writer); // primeira leva de dados = 5 registros
-        // desconexão aqui
-        
-        app.conexao = app.simInputData(app.conexao, 3, writer); // segunda leva de dados = 7 registros
-        // reconexão aqui 
-        
-        
-        
-        
-        writer.close(); /* encerra o txt e salva nele, isso aqui eu to testando ainda, ele n deve ficar 
-                na class input pq se quiser botar mais de 2 inputs offline ele lá vai apagar o 1º*/
-        
+        app.setConexao(true);
+            
+
     }
 
 }
