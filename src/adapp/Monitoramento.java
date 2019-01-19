c279fdc2f3e464003eba1cd2db76fca66c4be60b
Caio Lobato <Caio Lobato@DESKTOP-VVC9103>
2019-01-19 04:26:23 -0300

    final

--- src/adapp/Monitoramento.java
+++ src/adapp/Monitoramento.java
@@ -5,10 +5,47 @@
  */
 package adapp;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Caio Lobato
  */
-public class Monitoramento {
-    
+public class Monitoramento implements Observer {
+
+    Principal app;// boolean m_conexao;
+
+    public Monitoramento(Principal p) {
+        this.app = p;
+        //m_conexao = null;
+        System.out.println("Monitoramento de Conexão criado");
+    }
+
+    @Override
+    public void update(Observable obs, Object arg) {
+        
+        if(app.getConexao()==true) {
+            System.out.println("Conexão = ON");
+
+            String tempQuery ="";
+            try {
+                tempQuery = app.readFile("temp.txt");
+            } catch (IOException ex) {
+                Logger.getLogger(Monitoramento.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                        
+            app.insertFuncionarioFromTxt(tempQuery);
+        }
+        else if (app.getConexao()==false) System.out.println("Conexão = OFF");;               
+                
+        
+        
+    }
+
 }
